a14cd240c6629229a9773002ee05e8ba0fd7e6c1
irman <irmansyah.mohammad@gmail.com>
2021-10-12 16:47:56 +0700

    vercel mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify';
-/** @type {import('@sveltejs/kit').Config} */
+import vercel from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-netlify';
+// /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
+		// adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
